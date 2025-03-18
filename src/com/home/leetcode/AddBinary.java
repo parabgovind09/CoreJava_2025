d2aef83cafd34dca0819003d0342033c459ff11e
Govind Parab <vishnuparab0909@gmail.com>
2025-03-18 12:20:11 +0530

    LeetCode AddBinary Solved

---
+++ src/com/home/leetcode/AddBinary.java
@@ -0,0 +1,36 @@
+package com.home.leetcode;
+/*
+Add Binary
+Given two binary strings a and b, return their sum as a binary string.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101" 
+ */
+public class AddBinary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution3 obj = new Solution3();
+		System.out.println(obj.addBinary("1001", "11001"));
+		System.out.println(obj.addBinary("100111111", "11111111"));
+	}
+}
+class Solution3 {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder("");
+        int i = a.length()-1;
+        int j = b.length()-1;
+        byte carry = 0;
+        while(i >= 0 || j >=0 || carry > 0){
+            int sum = carry;
+            if(i>=0) sum += a.charAt(i--)-'0';
+            if(j>=0) sum += b.charAt(j--)-'0';
+            sb = sb.append((sum%2));
+            carry = (byte) (sum/2);
+        }
+        return sb.reverse().toString();
+    }
+}
